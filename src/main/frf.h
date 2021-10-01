a8ae31dde0985bc196bc6948b8434ef441b0e806
eso <eso@esotericist.org>
2021-09-30 23:48:00 -0700

    header stubs. don't judge me.

--- src/main/frf.h
+++ src/main/frf.h
@@ -1,1 +1,52 @@
 #include <stdbool.h>
+
+struct node_state {
+    /**
+     * int nextPID
+     * map int atomtoprimtable
+     * map int primtoatomtable
+     * 
+     * map int atomtowordtable
+     * map int wordtoatomtable
+     * 
+     * map string defines
+     * 
+     * process_state interpreter_process
+     * 
+     * map process_state process_pool
+     * 
+     * list int activeprocesses
+     * list int inactiveprocesses
+     * list int deadprocesses
+     * 
+     */ 
+
+
+};
+
+struct process_state {
+    /**
+     * 
+     * int pid
+     * node_state node
+     * 
+     * int parsemode
+     * int errorstate
+     * int executestate
+     * 
+     * int processlistptr -- pointer to this process' position in the relevant process list of the owning node
+     * 
+     * int currentop
+     * code_stream current_codestream
+     * 
+     */    
+};
+
+struct code_set {
+    /**
+     * 
+     */
+
+};
+
+#define str_eq( a, b ) ( sdscmp( (a), (b) ) == 0)
