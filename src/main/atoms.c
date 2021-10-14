76cf82f1d09fe2fc3ac5d4341a474807c6f5d41f
eso <eso@esotericist.org>
2021-10-13 20:38:48 -0700

    sanitizeatomstring now with 98% less undefined behavior

--- src/main/atoms.c
+++ src/main/atoms.c
@@ -31,7 +31,7 @@ sds sanitizeatomstring( sds str ) {
             case '0' ... '9':
             case '_':
             case 'a' ... 'z': {
-                sdscat(outstr, sdsnewlen( &c, 1 ) );
+                outstr = sdscatlen(outstr, &c, 1 );
                 continue;
             }
         }
