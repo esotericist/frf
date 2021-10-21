a969fcebd648d753a91aaf421da37e13cc9627d7
eso <eso@esotericist.org>
2021-10-21 16:36:37 -0700

    remove a bunch of no-longer-useful test logic

--- src/main/frf.c
+++ src/main/frf.c
@@ -54,33 +54,8 @@ int main(int argc, char **argv) {
 
     P->parsemode.flags = 0;
     sds input = readfile( P );
-    // sdstolower ( input );
+    sdstolower ( input );
 
-    /*
-    uintptr_t push_i = (uintptr_t)(void *) fetchprim( N, a_push_int );
-    uintptr_t pop_i = (uintptr_t)(void *) fetchprim( N, a_pop_int );
-    append_cp( P, push_i );
-    append_cp( P, 25 );
-    append_cp( P, push_i );
-    append_cp( P, -3 );
-    append_cp( P, push_i );
-    append_cp( P, 15 );
-    append_cp( P, push_i );
-    append_cp( P, 0 );
-    append_cp( P, pop_i );
-    append_cp( P, pop_i );
-    append_cp( P, pop_i );
-    append_cp( P, pop_i );
-    */
-    /*
-    P->current_codestream->codestream[0].p_val = fetchprim( N, a_push_int ) ;
-    P->current_codestream->codestream[1].u_val = 32;
-    P->current_codestream->codestream[2].p_val = fetchprim( N, a_push_int ) ;
-    P->current_codestream->codestream[3].u_val = -10;
-    P->current_codestream->codestream[4].p_val = fetchprim( N, a_push_int ) ;
-    P->current_codestream->codestream[5].u_val = 7;
-    P->current_codestream->length=6;
-    */
 
     P->currentop = 0;
     while( P->current_codestream->instructioncount > 0 ) {
@@ -107,54 +82,6 @@ int main(int argc, char **argv) {
     }
 
     printf("\n%s\n", dump_stack( P) );
-    // print_stack( P );
-
-
-    // struct process_state *p = GC_malloc( sizeof( struct process_state ) );
-    /*
-    push_int(P, 5 );
-    push_int(P, -6 );
-    push_int(P, 7 );
-
-    
-
-    printf( "%ld, ", pop_int( P ) );
-    printf( "%ld, ", pop_int( P ) );
-    printf( "%ld, ", pop_int( P ) );
-
-    printf( "\n");
-    */
-
-
-    
-
-     struct sglib_hashed_sListType_iterator it;
-     struct slist *nn, *ll;
-
-     sListType *table[slist_hash_size];
-
-     sglib_hashed_sListType_init( table );
-
-     nn = GC_malloc( sizeof(struct slist ) );
-     nn->s = sdsnew( "asdf" );
-
-    sglib_hashed_sListType_add( table, nn );
-
-    for( ll=sglib_hashed_sListType_it_init(&it, table);ll!=NULL; ll=sglib_hashed_sListType_it_next(&it)) {
-        printf("%s, %zu\n", ll->s, ll->data );
-    }
-
-     // struct sglib_hashed_slist_iterator it;
-
-     size_t i = newatom( sdsnew("asdf"));
-     newatom( sdsnew( "1234" ));
-     newatom( sdsnew( "abcd" ) );
-     printf( "%zu, %s\n", i,  atomtostring( i ) );
-     printf( "%i, %s\n", 3, atomtostring( 3));
-     i = verifyatom( sdsnew ( "1234" ) );
-     printf( "%zu, %s\n", i,  atomtostring( i ) );
-
-    printf( "%s, %s ", atomtostring( a_foo ), atomtostring( a_bar ) );
 
      return 0;
 }
