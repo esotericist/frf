82c5f63e622ceae9fa0121ab24d86470f760e7b6
eso <eso@esotericist.org>
2021-11-18 00:55:33 -0800

    answer: getline is exceedingly platform dependant.

--- src/main/files.c
+++ src/main/files.c
@@ -1,6 +1,8 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
 #include <unistd.h>
 #include <dirent.h>
 #include "stack.h"
@@ -10,9 +12,63 @@
 #include "prims.h"
 
 
+// swiped from https://stackoverflow.com/a/47229318 on 2021/11/18
+//
+/* The original code is public domain -- Will Hartung 4/9/09 */
+/* Modifications, public domain as well, by Antti Haapala, 11/10/17
+   - Switched to getc on 5/23/19 */
+
+ssize_t getline(char **lineptr, size_t *n, FILE *stream) {
+    size_t pos;
+    int c;
+
+    if (lineptr == NULL || stream == NULL || n == NULL) {
+        errno = EINVAL;
+        return -1;
+    }
+
+    c = getc(stream);
+    if (c == EOF) {
+        return -1;
+    }
+
+    if (*lineptr == NULL) {
+        *lineptr = malloc(128);
+        if (*lineptr == NULL) {
+            return -1;
+        }
+        *n = 128;
+    }
+
+    pos = 0;
+    while(c != EOF) {
+        if (pos + 1 >= *n) {
+            size_t new_size = *n + (*n >> 2);
+            if (new_size < 128) {
+                new_size = 128;
+            }
+            char *new_ptr = realloc(*lineptr, new_size);
+            if (new_ptr == NULL) {
+                return -1;
+            }
+            *n = new_size;
+            *lineptr = new_ptr;
+        }
+
+        ((unsigned char *)(*lineptr))[pos ++] = c;
+        if (c == '\n') {
+            break;
+        }
+        c = getc(stream);
+    }
+
+    (*lineptr)[pos] = '\0';
+    return pos;
+}
+
+
 #pragma GCC push_options
 #pragma GCC optimize("align-functions=16")
-
 
 // #region file manipulation prims
 
@@ -181,6 +237,8 @@ prim(readline) {
         push_string(sfsempty());
     }
 }
+
+
 
 prim2(print, .)
 {
