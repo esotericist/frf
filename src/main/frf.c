2d88c7d49804f541ad070ea7e33e805753f5188e
eso <eso@esotericist.org>
2024-03-03 21:11:14 -0800

    allow invoking arbitrary .frf files on cmd line
    also better error checking for invalid/inaccessible files

--- src/main/frf.c
+++ src/main/frf.c
@@ -15,25 +15,22 @@
 
 atom(exit)
 
-sfs readfile( proc *P ) {
-    sfs to_return = sfsempty();
+// todo: once we're able to trigger compilation via frf (and have
+// and embedded bootstrap) toss this entirely.
+void parsefile( proc *P, sfs filepath ) {
+    sfs to_process;
 
      char buffer[BUFSIZ];
 
-     getcwd(buffer, BUFSIZ);
-     sfs filepath = sfscatsfs( sfsnew(buffer), sfsnew( "/tests/prototype.frf" ) );
      FILE *thefile;
      thefile = fopen( filepath, "r" );
 
      while( fgets( buffer, BUFSIZ, thefile ) != NULL && P->errorstate == 0 ) {
-         to_return = sfsnew( buffer );
-         parse_line( P, to_return);
-
+         to_process = sfsnew( buffer );
+         parse_line( P, to_process);
      }
      
      fclose(thefile);
-
-     return to_return;
 }
 
 extern sfs numstring;
@@ -54,16 +51,32 @@ void frf_teardown() {
     events_teardown();
 }
 
-void node_bootstrap() {
+void node_bootstrap( sfs filepath ) {
     N = newnode();
     finalizeprims( N );
+
+    if (sfslen( sfstrim( filepath, " " ) ) == 0 ) {
+        filepath = sfsnew( "tests/prototype.frf" );
+    }
+
+    uv_fs_t accessreq;
+    uv_fs_t statreq;
+
+    int access = uv_fs_access( uvloop, &accessreq, filepath, O_RDONLY, 0 );
+    int stat = uv_fs_stat( uvloop, &statreq, filepath, 0 );
+
+    if ( access || stat || S_ISDIR( statreq.statbuf.st_mode ) ) {
+        uv_fs_req_cleanup( &accessreq );
+        uv_fs_req_cleanup( &statreq );
+            printf( "no valid frf file. aborting.\n\n" );
+            return;
+    }
 
     proc *P = newprocess( N );
     newcompilestate( P );
     P->current_codestream = newcodeset(N, 1024, 0 );
 
-    sfs input = readfile( P );
-    sfstolower ( input );
+    parsefile( P, filepath );
 
 }
 
@@ -77,7 +90,10 @@ void node_run() {
 int main(int argc, char **argv) {
     frf_initialization();
 
-    node_bootstrap();
+    // in the future we'll want to do more advanced argument processing
+    // but i don't want to do that in c. maybe in the future where i have
+    // an embedded frf bootstrap thing i can do smarter stuff.
+    node_bootstrap( sfsnew ( argv[1] ) );
     node_run();
 
     frf_teardown();
